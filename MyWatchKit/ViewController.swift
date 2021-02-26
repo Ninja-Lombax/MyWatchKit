a9f147cf344c62061e4428b7e577fc7a027998a9
ninjalombax <godzillakotm80@gmail.com>
2021-02-26 14:05:44 -0600

    Initial Commit

---
+++ MyWatchKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWatchKit
+//
+//  Created by John Grasser on 2/26/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
